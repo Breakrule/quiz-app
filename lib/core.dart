1061010b55fd678dc3bb4c17a3a78c14c49a0719
rezky <rezkymaulanagt@gmail.com>
2023-10-22 20:58:59 +0800

    result screen

---
+++ lib/core.dart
@@ -0,0 +1,14 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:quiz_app_rez/answer_button.dart';
+export 'package:quiz_app_rez/data/questions.dart';
+export 'package:quiz_app_rez/models/quiz_questions.dart';
+export 'package:quiz_app_rez/questions_screen.dart';
+export 'package:quiz_app_rez/questions_summary.dart';
+export 'package:quiz_app_rez/quiz.dart';
+export 'package:quiz_app_rez/results_screen.dart';
+export 'package:quiz_app_rez/start_screen.dart';
+export 'package:quiz_app_rez/state_util.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:quiz_app_rez/core.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app_rez/quiz.dart';
 
